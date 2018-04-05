ddd3a4c1e7f190009139b5fcc1e4588ed98fe37a
asmodianx <asmodianx@users.noreply.github.com>
2018-04-05 13:17:43 -0500

    cleaned up tabbing and moved gpsd section

--- Testing/gpsd_syslog.py
+++ Testing/gpsd_syslog.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
-
 ## *** UDP SYSLOG Server that appends GPSD location ***
 
-
+#import libraries
 import os
 from gps import *
 from time import *
@@ -11,40 +10,44 @@ import threading
 import logging
 import SocketServer
 
+#set configuration
+LOG_FILE = 'gps_433wipi_logs.log'
+HOST, PORT = "127.0.0.1", 2514
+
+#start gps poller in thread
 gpsd = None #seting the global variable
+gpsp = GpsPoller()
+gpsp.start()
 
 os.system('clear') #clear the terminal (optional)
 
-LOG_FILE = 'gps_433wipi_logs.log'
-HOST, PORT = "0.0.0.0", 2514
-
-
-class GpsPoller(threading.Thread):
-  def __init__(self):
-    threading.Thread.__init__(self)
-    global gpsd #bring it in scope
-    gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
-    self.current_value = None
-    self.running = True #setting the thread running to true
-
-  def run(self):
-    global gpsd
-    while gpsp.running:
-      gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
-
+#configure logging
 logging.basicConfig(level=logging.INFO, format='%(message)s', datefmt='', filename=LOG_FILE, filemode='a')
 
-class SyslogUDPHandler(SocketServer.BaseRequestHandler):
+#GPS poller - starts thread that starts wild loop updating next gps value in gpsd object
+class GpsPoller(threading.Thread):
+	def __init__(self):
+	threading.Thread.__init__(self)
+	global gpsd #bring it in scope
+		gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
+		self.current_value = None
+		self.running = True #setting the thread running to true
 
+	def run(self):
+		global gpsd
+		while gpsp.running:
+			gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
+
+#syslog udp handler - opens configured port and updates data object with incomming syslog message and appends gpsd lat and long and writes to log file
+
+class SyslogUDPHandler(SocketServer.BaseRequestHandler):
 	def handle(self):
-		data = bytes.decode(self.request[0].strip()) + str(gpsd.fix.longitude) + "," + str(gpsd.fix.latitude) + " "
+		data = bytes.decode(self.request[0].strip()) + " " +str(gpsd.fix.latitude) + "," + str(gpsd.fix.longitude)
 		socket = self.request[1]
 		print( "%s : " % self.client_address[0], str(data))
 		logging.info(str(data))
 
 if __name__ == "__main__":
-	gpsp = GpsPoller()
-	gpsp.start()
 	try:
 		server = SocketServer.UDPServer((HOST,PORT), SyslogUDPHandler)
 		server.serve_forever(poll_interval=0.5)
