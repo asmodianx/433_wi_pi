30695cde928e4d93029f865fe656f7551d3db760
asmodianx <asmodianx@users.noreply.github.com>
2018-04-04 21:10:50 -0500

    Update record_shutdown.sh
    removed redundant log file saving script and integrated into shutdown script.

--- record_shutdown.sh
+++ record_shutdown.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 BUTTON=`/usr/local/bin/button.py`
+date=`date +%Y%m%d_%H%M`
+
 if [ -f /tmp/shutdown ]; then
         echo "Already shutting down"
 else
         touch /tmp/shutdown
         if [ $BUTTON = "1" ]; then
+                service kismet stop
+                service rtl_433 stop
                 mount /mnt
-                rm /mnt/*.tgz
-                /root/logs.sh
+                rm /mnt/*.tar
+                tar -cvf /mnt/$date.tar /var/log/.
                 sync
                 umount /mnt
                 sync
                 rm /tmp/shutdown
-                halt -p
+                /sbin/halt -p
         else
                 echo "Button is $BUTTON, Not shutting down"
         fi
