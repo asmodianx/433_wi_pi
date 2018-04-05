738ad8a45202511ebc48425b76250eb007e3e268
asmodianx <asmodianx@users.noreply.github.com>
2018-04-05 09:32:14 -0500

    Update record_shutdown.sh
    because seckc

--- record_shutdown.sh
+++ record_shutdown.sh
@@ -9,12 +9,16 @@ else
         if [ $BUTTON = "1" ]; then
                 service kismet stop
                 service rtl_433 stop
+                killall rtl_433
+                killall kismet_server
                 mount /mnt
                 rm /mnt/*.tar
                 tar -cvf /mnt/$date.tar /var/log/.
                 sync
                 umount /mnt
                 sync
+                #The Gps will take time to start up and get a fix while it does so everything will appear to be at kanza hall for a seckc meeting.
+                echo "38.9130449,-94.6712426">/tmp/gps.txt
                 rm /tmp/shutdown
                 /sbin/halt -p
         else
